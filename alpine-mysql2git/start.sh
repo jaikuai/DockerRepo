7eb8e449da61723610f6e15c9e3f57d662be0980
jaikuai <jaikuai@gmail.com>
2019-05-13 18:29:17 +0800

    update

--- alpine-mysql2git/start.sh
+++ alpine-mysql2git/start.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 
-git clone --depth=1 https://$GIT_USER:$GIT_PASS@git.oschina.net$GIT_URL data \
-	&& cd data && rm -rf $MY_DIR && mkdir -p $MY_DIR \
-	&& mysqldump -hmysql -ubackup -p$MYSQL_PASSWORD --opt --all-databases |gzip > $MY_DIR/all.db.sql.gz \
-	&& git add $MY_DIR/all.db.sql.gz \
-	&& git commit -m $(date '+%Y-%m-%d%H:%M:%S') \
+git clone --depth=1 $GIT_URL data \
+	&& cd data && rm -f $FILE_PATH \
+	&& mysqldump -hmysql -uroot -p$MYSQL_PASSWORD --opt --all-databases |gzip > $FILE_PATH \
+	&& git add $FILE_PATH \
+	&& git commit -m $(date '+%Y-%m-%d %H:%M:%S') \
 	&& git push origin master \
 	&& rm -rf ../data
 
