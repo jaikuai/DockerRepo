bf36a539d48c60a37d0dc94f90ed1dbb9320c3ce
jaikuai <jaikuai@gmail.com>
2022-03-27 17:47:55 +0800

    add tzdata

--- alpine-dnspod/App.py
+++ alpine-dnspod/App.py
@@ -25,32 +25,35 @@ def do():
     except Exception as e:
         print('%s 获取本机IP异常' % now)
         return
-    
-    ## Get Domain Info
-    param = urllib.parse.urlencode({"login_token": token, "format": "json", "domain": domain})
-    data = urllib.request.urlopen("https://dnsapi.cn/Record.List", param.encode('ascii')).read().decode('UTF-8')
-    jdata = json.loads(data)
 
-    record_id = ""
-    record_ip = ""
-    for obj in jdata["records"]:
-        if obj["name"] == name and obj['status'] == 'enable' and obj['type'] == 'A':
-            record_id = obj["id"]
-            record_ip = obj["value"]
-            break
-    ## 判断是否更新
-    if record_ip == ip_data["cip"]:
-        print("%s IP相同 %s = %s，跳过" % (now, record_ip,ip_data["cip"]))
-        pass
-    else:
-        params = urllib.parse.urlencode({"login_token": token, "format": "json", "domain": domain, "record_id": record_id,"sub_domain": name, "record_type": "A", "record_line": "默认", "value": ip_data["cip"]})
-        data = urllib.request.urlopen("https://dnsapi.cn/Record.Modify", params.encode('ascii')).read().decode('UTF-8')
-        res = json.loads(data)
-        if res["status"]["code"] == "1" :
-            print("%s Update IP: %s" % (now, ip_data["cip"]))
+    try:
+        ## Get Domain Info
+        param = urllib.parse.urlencode({"login_token": token, "format": "json", "domain": domain})
+        data = urllib.request.urlopen("https://dnsapi.cn/Record.List", param.encode('ascii')).read().decode('UTF-8')
+        jdata = json.loads(data)
+
+        record_id = ""
+        record_ip = ""
+        for obj in jdata["records"]:
+            if obj["name"] == name and obj['status'] == 'enable' and obj['type'] == 'A':
+                record_id = obj["id"]
+                record_ip = obj["value"]
+                break
+        ## 判断是否更新
+        if record_ip == ip_data["cip"]:
+            print("%s IP相同 %s = %s，跳过" % (now, record_ip,ip_data["cip"]))
+            pass
         else:
-            print('%s %s' % (now, res["status"]["message"]))
-
+            params = urllib.parse.urlencode({"login_token": token, "format": "json", "domain": domain, "record_id": record_id,"sub_domain": name, "record_type": "A", "record_line": "默认", "value": ip_data["cip"]})
+            data = urllib.request.urlopen("https://dnsapi.cn/Record.Modify", params.encode('ascii')).read().decode('UTF-8')
+            res = json.loads(data)
+            if res["status"]["code"] == "1" :
+                print("%s Update IP: %s" % (now, ip_data["cip"]))
+            else:
+                print('%s %s' % (now, res["status"]["message"]))
+    except Exception as e:
+        print('%s 获取域名IP或更新时报错' % now)
+        return
 ## 定时任务
 timer = threading.Timer(int(wait_time), do)
 timer.start()
