be8c2aefce0850ee43658239627c869d3011edca
jaikuai <jaikuai@gmail.com>
2025-01-17 11:14:44 +0800

    init_fe

--- alpine-jre-apm/start.sh
+++ alpine-jre-apm/start.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-if [ "$PRO_ENV" != "uat" ]; then
-  /usr/bin/java -server -jar /home/${PRO_NAME}.jar --spring.profiles.active=${PRO_ENV}
-else
-  /usr/bin/java -server -javaagent:/home/pinpoint-agent-2.4.2/pinpoint-bootstrap-2.4.2.jar -Dpinpoint.agentId=${PRO_NAME} -Dpinpoint.applicationName=${PRO_NAME} -jar /home/${PRO_NAME}.jar --spring.profiles.active=${PRO_ENV}
-fi
+APM_NODE_ADDRESS="http://apm-node:9994"
 
+# 检查是否可以访问APM节点地址
+if curl --output /dev/null --silent --head --fail "$APM_NODE_ADDRESS"; then
+  /usr/bin/java -server -javaagent:/home/pinpoint-agent-2.4.2/pinpoint-bootstrap-2.4.2.jar -Dpinpoint.agentId=${PRO_NAME} -Dpinpoint.applicationName=${PRO_NAME} -jar /home/${PRO_NAME}.jar --spring.profiles.active=${PRO_ENV}
+else
+  /usr/bin/java -server -jar /home/${PRO_NAME}.jar --spring.profiles.active=${PRO_ENV}
+fi
